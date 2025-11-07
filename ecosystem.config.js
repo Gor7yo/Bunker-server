cad92d1e98eeaa6bedbd8ed1a35f6bf6f5ada9db
gor2210 <gor22.11@bk.ru>
2025-11-07 10:59:11 +0300

    -

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   apps: [{
     name: 'bunker-server',
-    script: './mediasoup-server.js',
+    script: './websocket.js',
     instances: 1,
     autorestart: true,
     watch: false,
